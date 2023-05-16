e964c9449f6c5ea170154756db028f5b2e03be29
Александр <hyperpositron@gmail.com>
2023-05-16 10:26:28 +0200

    first commit

---
+++ src/main/java/com/mokitoSpringGroup/mokitoSpringArtifact/MokitoSpringArtifactNameApplication.java
@@ -0,0 +1,13 @@
+package com.mokitoSpringGroup.mokitoSpringArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoSpringArtifactNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitoSpringArtifactNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mokitoSpringGroup/mokitoSpringArtifact/MokitoSpringArtifactNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokitoSpringGroup.mokitoSpringArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokitoSpringArtifactNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
